fc188a27bb4362d2773173959d6268426629cbe1
JRamos29 <malebolge@gmail.com>
2021-10-27 19:32:41 -0300

    Implemented Static Site Generation (SSG) with getStaticProps.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <h1> Hello! </h1>;
+import P from 'prop-types';
+import styled from 'styled-components';
+import config from '../config';
+import { mapData } from '../api/map-data';
+import Home from '../templates/Home';
+
+export default function Index({ data = null }) {
+  return <Home data={data} />;
 }
+
+export const getStaticProps = async () => {
+  const raw = await fetch(config.url + config.defaultSlug);
+  const json = await raw.json();
+  const data = mapData(json);
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+Index.propTypes = {
+  data: P.array,
+};
